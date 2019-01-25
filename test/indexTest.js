c499d270013c7ac1b9be28bcbccdea262f44282c
maxwellbenton <maxwellbenton@gmail.com>
2019-01-25 16:05:01 -0500

    initial test experiment

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,23 +1,26 @@
+let chai = require( "chai" );
+let chaiAsPromised = require( "chai-as-promised" );
 
+chai.use( chaiAsPromised );
 
-describe("index.html", () => {
-  it("includes 'hello'", () => {
-    expect(document.querySelector('body').innerHTML).to.include('hello')
-  })
+describe( 'index.js', () => {
 
-  it("includes div", () => {
-    expect(document.querySelector('body').innerHTML, "no empty div element found within body").to.include('<div></div>')
-  })
+  describe( 'myFetch()', () => {
+    let astronauts
+    let results
+    before( () => {
+      window.fetch = require( 'node-fetch' )
+    } );
 
+    it( '1', async () => {
+      astronauts = window.fetch( 'http://api.open-notify.org/astros.json' ).then( response => response.json() ).then( json => `${json[ "number" ]}` )
+      results = myFetch( 'http://api.open-notify.org/astros.json' ).then( response => response.slice( response.indexOf( '"number":' ), response.indexOf( '"number":' ) + 12 ).match( /[0-9]+/ )[ 0 ] )
+      await astronauts
+      await results
 
-})
+      console.log( astronauts, results );
+      expect( astronauts ).to.eql( results )
+    } );
+  } );
 
-
-describe("index.js", () => {
-  it("exists", () => {
-    expect(testVar).to.exist
-    expect(testVar).to.deep.equal({}) //without .deep, {} does not equal {}
-    expect(testFunc).to.exist
-    expect(testFunc()).to.equal('hi')
-  })
-})
+} );
